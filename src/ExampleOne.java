aedeb0bea9f3c0cf4bbbecbd259493a44b543354
Jorge Fernandez <jorgelfc1989@gmail.com>
2022-02-10 16:59:01 -0500

    first commit

---
+++ src/ExampleOne.java
@@ -0,0 +1,13 @@
+
+public class ExampleOne {
+
+	public static void main(String[] args) {
+		if(true) {
+			System.out.println("Inside the if-statement");
+			boolean isOn= false;
+			if (isOn){
+				System.out.println("The Variable 'isOn' was true");
+			}
+		}
+	}
+}
